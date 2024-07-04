e342ef1218455370616c902163a5b1f5e0c59d12
GldeA <glde.abbott@gmail.com>
2024-07-04 18:57:49 -0500

    Implemented a death zone below the stage and level borders

---
+++ scripts/DeathBoundary.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@export var player: CharacterBody2D
+@export var start: Vector2 # Where the player starts in the level, must be updated on level change
+
+signal player_died # currently unused
+
+func _on_body_entered(body):
+	if body.name == player.name:
+		player.position = start
+		player_died.emit()
