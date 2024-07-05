fe0047ea86355acf45bc32a9cd83ead81586e90a
GldeA <glde.abbott@gmail.com>
2024-07-05 09:15:01 -0500

    Added particles on player death

--- scripts/DeathBoundary.gd
+++ scripts/DeathBoundary.gd
@@ -2,10 +2,13 @@ extends Area2D
 
 @export var player: CharacterBody2D
 @export var home: Marker2D # Where the player starts in the level, must be updated on level change
+@export var death_particles: GPUParticles2D
 
 signal player_died # currently unused
 
 func _on_body_entered(body):
 	if body.name == player.name:
-		player.position = home.position
 		player_died.emit()
+		death_particles.position = player.position
+		death_particles.emitting = true
+		player.position = home.position
