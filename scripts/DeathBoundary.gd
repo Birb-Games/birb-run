a3949d83cdefc914c4a14f8f1623a0c48058559b
GldeA <glde.abbott@gmail.com>
2024-07-04 19:13:09 -0500

    Implemented a home point for respawn

--- scripts/DeathBoundary.gd
+++ scripts/DeathBoundary.gd
@@ -1,11 +1,11 @@
 extends Area2D
 
 @export var player: CharacterBody2D
-@export var start: Vector2 # Where the player starts in the level, must be updated on level change
+@export var home: Marker2D # Where the player starts in the level, must be updated on level change
 
 signal player_died # currently unused
 
 func _on_body_entered(body):
 	if body.name == player.name:
-		player.position = start
+		player.position = home.position
 		player_died.emit()
