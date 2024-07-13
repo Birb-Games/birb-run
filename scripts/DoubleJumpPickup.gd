699a488d55476265a478b32204e3f9454a04f103
JL <jeremyli.gitemail@gmail.com>
2024-07-13 18:11:44 -0500

    updated appearance of double jump pickup

--- scripts/DoubleJumpPickup.gd
+++ scripts/DoubleJumpPickup.gd
@@ -1,18 +1,17 @@
 extends Area2D
 
-@onready var noise_texture = $Sprite2D.material.get_shader_parameter("noise").noise
+@onready var noise_texture = $AnimatedSprite2D.material.get_shader_parameter("noise").noise
 
-const NOISE_VERTICAL_SCROLL_SPEED = 0.35
+const NOISE_VERTICAL_SCROLL_SPEED = 30.0
 
 func _ready():
 	if $/root/Root.player.double_jump_unlocked:
 		queue_free()
 
 func _process(delta):
-	noise_texture.offset.y += NOISE_VERTICAL_SCROLL_SPEED
+	noise_texture.offset.y += NOISE_VERTICAL_SCROLL_SPEED * delta
 
 func _on_body_entered(body):
-	print(body.name)
 	if body is Player:
 		body.double_jump_unlocked = true
 		body.get_node("PowerupAudioPlayer").play()
