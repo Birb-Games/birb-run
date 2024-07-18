519f8f8ae0a5ec68c8d5e75e63f7ac222bf680c4
GldeA <glde.abbott@gmail.com>
2024-07-17 22:12:37 -0500

    Added size and lifetime variance to make checkpoint particles more lively

--- scripts/Checkpoint.gd
+++ scripts/Checkpoint.gd
@@ -6,7 +6,8 @@ func _on_body_entered(body):
 		$"../Home".position = position
 		$GPUParticles2D.process_material.radial_velocity_max = 300
 		$GPUParticles2D.process_material.radial_velocity_min = 100
-		$GPUParticles2D.lifetime = 3
+		$GPUParticles2D.process_material.lifetime_randomness = 1
+		$GPUParticles2D.lifetime = 1
 		$GPUParticles2D.amount = 100
 		$GPUParticles2D.one_shot = true
 		$GPUParticles2D.explosiveness = 1
