5523589541d74b696b26942a7cfc77da32cf3434
JL <jeremyli.gitemail@gmail.com>
2024-07-18 12:23:03 -0500

    added artwork for checkpoint and sound effect for when you arrive at a checkpoint

--- scripts/Checkpoint.gd
+++ scripts/Checkpoint.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
+func _ready():
+	$AnimatedSprite2D.hide()
+
 func _on_body_entered(body):
-	print(body.name)
 	if body is Player:
 		$"../Home".position = position
 		$GPUParticles2D.process_material.radial_velocity_max = 300
@@ -11,4 +13,9 @@ func _on_body_entered(body):
 		$GPUParticles2D.amount = 100
 		$GPUParticles2D.one_shot = true
 		$GPUParticles2D.explosiveness = 1
-		monitoring = false
+		set_deferred("monitoring", false)
+		
+		if !$AnimatedSprite2D.visible:
+			$AudioStreamPlayer2D.play()
+			
+		$AnimatedSprite2D.show()
